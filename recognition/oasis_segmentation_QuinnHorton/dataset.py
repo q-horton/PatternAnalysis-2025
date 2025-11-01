a325b5b41fd6bed6b9025a8d86c5e5eb37129952
q-horton <106427075+q-horton@users.noreply.github.com>
2025-11-01 13:54:38 +1000

    Added pytorch-based tensor loading of the image data.

--- recognition/oasis_segmentation_QuinnHorton/dataset.py
+++ recognition/oasis_segmentation_QuinnHorton/dataset.py
@@ -1,18 +1,30 @@
 from PIL import Image
 import os
-import numpy as np
+import torch
+from torchvision import datasets, transforms
 from dotenv import load_dotenv
 
+# Load relevant environment variables
 load_dotenv()
 OASIS_PATH = os.getenv('OASIS_PATH')
 
-training_files = os.listdir(f"{OASIS_PATH}/keras_png_slices_seg_train")
-image = Image.open(f"{OASIS_PATH}/keras_png_slices_seg_train/{training_files[0]}")
+# Load dataset for usage
+transform = transforms.Compose([
+    transforms.ToTensor()
+    ])
+training_files_dir = f"{OASIS_PATH}"  # /keras_png_slices_seg_train"
+training_files = datasets.ImageFolder(training_files_dir, transform=transform)
 
-print(image.format)
-print(image.size)
-print(image.mode)
+# Create a DataLoader to assist in the batching process
+dataloader = torch.utils.data.DataLoader(training_files, batch_size=32,
+                                         shuffle=True)
 
-list = np.array([image])
-
-image.show()
+# See things
+for images, labels in dataloader:
+    print(f"Image batch shape: {images.shape}")
+    print(f"Label batch shape: {labels.shape}")
+    print(f"Image shape: {images[0][0].shape}")
+    picture = transforms.functional.to_pil_image(images[0][0])
+    picture.show()
+    print(labels[0])
+    break
